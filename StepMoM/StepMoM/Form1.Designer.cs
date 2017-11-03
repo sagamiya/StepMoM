6326d424f2bb7ebd95ec04729d9cf783d29c1426
sagamiya <semina.rerum@gmail.com>
2017-11-03 20:57:56 +0900

    Initial release version 0.1.

---
+++ StepMoM/StepMoM/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace StepMoM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.sourcePath = new System.Windows.Forms.TextBox();
+            this.selectSrcButton = new System.Windows.Forms.Button();
+            this.statusArea = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.variable0From = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.variable0to = new System.Windows.Forms.TextBox();
+            this.step = new System.Windows.Forms.Label();
+            this.variable0step = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(51, 279);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(130, 27);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // sourcePath
+            // 
+            this.sourcePath.Location = new System.Drawing.Point(148, 66);
+            this.sourcePath.Name = "sourcePath";
+            this.sourcePath.ReadOnly = true;
+            this.sourcePath.Size = new System.Drawing.Size(646, 19);
+            this.sourcePath.TabIndex = 1;
+            // 
+            // selectSrcButton
+            // 
+            this.selectSrcButton.Location = new System.Drawing.Point(51, 64);
+            this.selectSrcButton.Name = "selectSrcButton";
+            this.selectSrcButton.Size = new System.Drawing.Size(75, 23);
+            this.selectSrcButton.TabIndex = 2;
+            this.selectSrcButton.Text = "Reference...";
+            this.selectSrcButton.UseVisualStyleBackColor = true;
+            this.selectSrcButton.Click += new System.EventHandler(this.selectSrcButton_Click);
+            // 
+            // statusArea
+            // 
+            this.statusArea.Location = new System.Drawing.Point(33, 327);
+            this.statusArea.Multiline = true;
+            this.statusArea.Name = "statusArea";
+            this.statusArea.ReadOnly = true;
+            this.statusArea.Size = new System.Drawing.Size(761, 82);
+            this.statusArea.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Step1.  Select the .smm (modified .omm) file.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(29, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(471, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Step 2.  Specify the condition: start, stop and step value.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Start from";
+            // 
+            // variable0From
+            // 
+            this.variable0From.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.variable0From.Location = new System.Drawing.Point(123, 190);
+            this.variable0From.Name = "variable0From";
+            this.variable0From.Size = new System.Drawing.Size(100, 19);
+            this.variable0From.TabIndex = 7;
+            this.variable0From.Text = "-0.001";
+            this.variable0From.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.variable0From.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Stop at";
+            // 
+            // variable0to
+            // 
+            this.variable0to.Location = new System.Drawing.Point(331, 190);
+            this.variable0to.Name = "variable0to";
+            this.variable0to.Size = new System.Drawing.Size(100, 19);
+            this.variable0to.TabIndex = 9;
+            this.variable0to.Text = "0.001";
+            this.variable0to.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // step
+            // 
+            this.step.AutoSize = true;
+            this.step.Location = new System.Drawing.Point(465, 193);
+            this.step.Name = "step";
+            this.step.Size = new System.Drawing.Size(28, 12);
+            this.step.TabIndex = 10;
+            this.step.Text = "Step";
+            // 
+            // variable0step
+            // 
+            this.variable0step.Location = new System.Drawing.Point(529, 190);
+            this.variable0step.Name = "variable0step";
+            this.variable0step.Size = new System.Drawing.Size(100, 19);
+            this.variable0step.TabIndex = 11;
+            this.variable0step.Text = "0.001";
+            this.variable0step.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(29, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(262, 19);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Step 3.  Commece  calculation.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 431);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.variable0step);
+            this.Controls.Add(this.step);
+            this.Controls.Add(this.variable0to);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.variable0From);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusArea);
+            this.Controls.Add(this.selectSrcButton);
+            this.Controls.Add(this.sourcePath);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox sourcePath;
+        private System.Windows.Forms.Button selectSrcButton;
+        private System.Windows.Forms.TextBox statusArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox variable0From;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox variable0to;
+        private System.Windows.Forms.Label step;
+        private System.Windows.Forms.TextBox variable0step;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ StepMoM/StepMoM/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StepMoM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kickOmm(string ommPath)
+        {
+            //ProcessStartInfoオブジェクトを作成する
+            System.Diagnostics.ProcessStartInfo psi =
+                new System.Diagnostics.ProcessStartInfo();
+            //起動するファイルのパスを指定する
+            psi.FileName = ".\\omm.exe";
+            //コマンドライン引数を指定する
+            psi.Arguments = ommPath;
+
+            //アプリケーションを起動する
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(psi);
+            p.WaitForExit();
+        }
+
+
+        private void eval(float startValue, float stopValue, float stepValue)
+        {
+            if (System.IO.File.Exists(sourcePath.Text))
+            {
+                string targetDir = System.IO.Path.GetDirectoryName(sourcePath.Text) + "\\" + System.DateTime.Now.ToString("yyyymmdd-HHmmss");
+                System.IO.Directory.CreateDirectory(targetDir);
+                float value1 = startValue;
+                for (int i=0; (value1 = startValue + stepValue*i) <= stopValue; i++) {
+                    string targetPath = targetDir + "\\"
+                        + System.IO.Path.GetFileNameWithoutExtension(sourcePath.Text) + "-" + value1.ToString("e4") + ".omm";
+                    System.IO.StreamReader sr = new System.IO.StreamReader(sourcePath.Text);
+                    System.IO.StreamWriter sw = new System.IO.StreamWriter(targetPath);
+                    string tmp;
+                    while ((tmp = sr.ReadLine()) != null)
+                    {
+                        sw.WriteLine(tmp);
+                    }
+                    sr.Close();
+                    sw.Close();
+                    kickOmm(targetPath);
+                }
+            } else
+            {
+                statusArea.Text = statusArea.Text + "\r\nErr: File not found.";
+            }
+        }
+
+        private void copyGeneratedFiles(string ommPath, string targetDir, string variable)
+        {
+            string tmp;
+            System.IO.StreamReader sr;
+            System.IO.StreamWriter sw;
+            {
+                sr = new System.IO.StreamReader(ommPath);
+                sw = new System.IO.StreamWriter(targetDir + "\\source " + variable + ".omm");
+                while ((tmp = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(tmp);
+                }
+                sr.Close();
+                sw.Close();
+            }
+
+            if (System.IO.File.Exists("current.log"))
+            {
+                sr = new System.IO.StreamReader("current.log");
+                sw = new System.IO.StreamWriter(targetDir + "\\current " + variable + ".log");
+                while ((tmp = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(tmp);
+                }
+                sr.Close();
+                sw.Close();
+            }
+            if (System.IO.File.Exists("element.log")) {
+                sr = new System.IO.StreamReader("element.log");
+                sw = new System.IO.StreamWriter(targetDir + "\\element " + variable + ".log");
+                while ((tmp = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(tmp);
+                }
+                sr.Close();
+                sw.Close();
+            }
+            if (System.IO.File.Exists("ev2d.htm"))
+            {
+                sr = new System.IO.StreamReader("ev2d.htm");
+                sw = new System.IO.StreamWriter(targetDir + "\\ev2d " + variable + ".htm");
+                while ((tmp = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(tmp);
+                }
+                sr.Close();
+                sw.Close();
+            }
+            if (System.IO.File.Exists("geom3d.htm"))
+            {
+                sr = new System.IO.StreamReader("geom3d.htm");
+                sw = new System.IO.StreamWriter(targetDir + "\\geom3d " + variable + ".htm");
+                while ((tmp = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(tmp);
+                }
+                sr.Close();
+                sw.Close();
+            }
+            if (System.IO.File.Exists("omm.log"))
+            {
+                sr = new System.IO.StreamReader("omm.log");
+                sw = new System.IO.StreamWriter(targetDir + "\\omm " + variable + ".log");
+                while ((tmp = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(tmp);
+                }
+                sr.Close();
+                sw.Close();
+            }
+        }
+
+        private void eval2(float startValue, float stopValue, float stepValue)
+        {
+            if (System.IO.File.Exists(sourcePath.Text))
+            {
+                string targetDir = System.IO.Path.GetDirectoryName(sourcePath.Text) + "\\" + System.IO.Path.GetFileNameWithoutExtension(sourcePath.Text)+" "+System.DateTime.Now.ToString("yyyymmdd-HHmmss");
+                System.IO.Directory.CreateDirectory(targetDir);
+                float value1 = startValue;
+                for (int i = 0; (value1 = startValue + stepValue * i) <= stopValue; i++)
+                {
+                    string ommPath = System.IO.Path.GetTempFileName();
+                    System.IO.StreamReader sr = new System.IO.StreamReader(sourcePath.Text);
+                    System.IO.StreamWriter sw = new System.IO.StreamWriter(ommPath);
+                    string line;
+                    string statement;
+                    Match mo;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        line = String.Format(line, value1);
+                        while (true) {
+                            mo = Regex.Match(line, "\"[^\"]*\"");
+                            if (mo.Success)
+                            {
+                                statement = line.Substring(mo.Index, mo.Length);
+                                System.Data.DataTable dt = new System.Data.DataTable();
+                                //line = line.Replace(mo.Value, statement.Substring(1, statement.Length - 2));
+                                //statusArea.Text = statusArea.Text + "\r\n" + statement;
+                                string result = (dt.Compute(statement.Substring(1, statement.Length - 2), "")).ToString();
+                                line = line.Replace(mo.Value, result.ToString() );
+                                //statusArea.Text = statusArea.Text + "\r\n" + line;
+                            } else
+                            {
+                                break;
+                            }
+                        }
+                        sw.WriteLine(line);
+                    }
+                    sr.Close();
+                    sw.Close();
+                    kickOmm(ommPath);
+                    copyGeneratedFiles(ommPath, targetDir, " "+i.ToString()+" " + value1.ToString("e4"));
+                }
+            }
+            else
+            {
+                statusArea.Text = statusArea.Text + "\r\nErr: File not found.";
+            }
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //eval2(0.024f, 0.026f, 0.001f);
+            float v0from, v0to, v0step;
+            v0from = float.Parse(variable0From.Text);
+            v0to = float.Parse(variable0to.Text);
+            v0step = float.Parse(variable0step.Text);
+            if (v0step != 0.0)
+            {
+                eval2(v0from, v0to, v0step);
+            }
+            statusArea.Text = statusArea.Text + "\r\nDone.";
+        }
+
+        private void selectSrcButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "StepMoMファイル(*.smm)|*.smm|OpenMoMファイル(*.omm)|*.omm|すべてのファイル(*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                sourcePath.Text = (openFileDialog1.FileName);
+            }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            double d;
+            if (!double.TryParse(((TextBox)sender).Text, out d))
+            {
+                // Cancel the event and select the text to be corrected by the user.
+                e.Cancel = true;
+                ((TextBox)sender).Select(0, ((TextBox)sender).Text.Length);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+    }
+}
